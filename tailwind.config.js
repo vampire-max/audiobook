7f0cb7ed0be18759f1890dc476f3b66316aca66f
jana6600 <jananikolic6600@gmail.com>
2021-11-03 05:08:26 -0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily:{
+        'Inter' : ['Inter', 'sans-serif']
+      },
+
+      colors: {
+        gray: {
+          100: "#C4C4C4",
+          800: "#292929"
+        }
+      },
+      
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
